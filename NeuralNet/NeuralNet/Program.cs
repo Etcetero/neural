377813929eea4cf05d07180bb88acca23f2f92e6
Semyon <one011001@gmail.com>
2021-03-08 22:43:50 +0300

    Project Created

---
+++ NeuralNet/NeuralNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
